bb37fb87676d2ab4db3bd0d3c4dfe209b1c9fae3
ganesh <gchimmula@gmail.com>
2025-04-10 13:33:57 -0400

    Add submit and dismiss button functionality

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,8 @@
+let newsletterCard = document.querySelector(".newsletter__signup-card");
+let successCard = document.querySelector(".success-card");
+let signUpButton = document.querySelector("#signup-button");
+let dismissButton = document.querySelector('#dismiss');
+let errorMessage = document.querySelector('#email-error');
 let emailInputElement = document.querySelector('#email');
 let emailValue = "";
 
@@ -5,4 +10,15 @@ let emailValue = "";
 emailInputElement.addEventListener('input', function (e) {
     emailValue = e.target.value;
     console.log(emailValue);
+})
+
+signUpButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    newsletterCard.classList.toggle('newsletter__signup-card--hidden')
+    successCard.classList.toggle('success-card--show')
+})
+
+dismissButton.addEventListener('click', function (e){
+    newsletterCard.classList.toggle('newsletter__signup-card--hidden')
+    successCard.classList.toggle('success-card--show')
 })
