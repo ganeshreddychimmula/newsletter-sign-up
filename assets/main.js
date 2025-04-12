60678e26e85886a039561e39e22784c5d19b8b3c
ganesh <gchimmula@gmail.com>
2025-04-12 14:33:24 -0400

    Update clss bassed on screen width

--- assets/main.js
+++ assets/main.js
@@ -47,3 +47,19 @@ emailInputElement.addEventListener('click', function (e){
 
 
 
+function updateClassBasedOnScreenWidth() {
+    const element = document.querySelector('.signup-card__form-details h2');
+    if (element) {
+      if (window.matchMedia('(min-width: 600px)').matches) {
+        element.classList.add('text-preset-1-desktop'); // Replace 'desktop-class' with your desired class
+      } else {
+        element.classList.remove('.text-preset-1-desktop');
+      }
+    }
+  }
+  
+  // Call the function initially
+  updateClassBasedOnScreenWidth();
+  
+  // Add an event listener for screen width changes
+  window.addEventListener('resize', updateClassBasedOnScreenWidth);
